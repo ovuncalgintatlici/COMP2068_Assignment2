17aabaeaedc7a5272f9bdfbf879f23ae5cace371
ovuncalgintatlici <43188371+ovuncalgintatlici@users.noreply.github.com>
2019-11-19 19:08:06 -0500

    first commit

---
+++ AdSite/models/item.js
@@ -0,0 +1,7 @@
+﻿const mongoose = require("mongoose");
+
+module.exports = mongoose.model("Items", new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number
+}));
---
+++ AdSite/routes/index.js
@@ -0,0 +1,48 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+const mongoose = require("mongoose");
+const Item = require("../models/item");
+
+// Mongo connect
+let mongo_url = "mongodb://localhost/itemdb"; // db connection string
+mongoose.connect(mongo_url)
+    .then(() => console.log("Db connection success"))
+    .catch(error => console.log("Db connection failed ", error.message));
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+// Add new item
+router.get('/addItem', function (req, res,next) {
+    let item = new Item({
+        title: "Item 1",
+        description: "description",
+        price: 50
+    });
+
+    item.save((error, data) => {
+        if (error) {
+            res.send("unexpected fail");
+        } else {
+            res.json(data);
+        }
+    });
+}); 
+
+router.get("/getItemList", (req, res, next) => {
+    Item.find({}, (error, data) => {
+        if (error) {
+            res.send("Unexpected fail");
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+
+module.exports = router;
